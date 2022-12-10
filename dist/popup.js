620b8d3cadd86987483492db9df409c1423e8f19
Yarin <15931306+TheYarin@users.noreply.github.com>
2022-12-10 22:00:24 +0200

    Add shortcuts table and tab count in current window to the popup

--- dist/popup.js
+++ dist/popup.js
@@ -1,3 +1,12 @@
-"use strict";
 // console.log(chrome.tabs);
 // alert("popup.js loaded");
+const commands = await chrome.commands.getAll();
+let html = "";
+for (const command of commands) {
+    html += `<tr><td>${command.name}</td><td>${command.shortcut}</td><td>${command.description}</td></tr>`;
+}
+html = `<table><thead><tr><th>NAME</th><th>SHORTCUT</th><th>DESCRIPTION</th></tr></thead><tbody>${html}</tbody></table>`;
+document.getElementById("shortcuts").innerHTML = html;
+const tabsInCurrentWindow = await chrome.tabs.query({ currentWindow: true });
+document.getElementById("tabs-in-current-window").innerHTML = tabsInCurrentWindow.length.toString();
+export {};
